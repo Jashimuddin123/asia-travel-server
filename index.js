f2bb1e6a50a05a3e07d84a67b65b1543f33e4a86
Jashimuddin123 <adnanmahmud3394@gmail.com>
2024-07-08 01:56:09 +0600

    changing some code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -44,7 +44,15 @@ async function run() {
       const result = await cursor.toArray()
       res.send(result)
   })
-
+    
+  // get method by _id
+  app.get("/touristSpots/:id", async(req, res)=>{
+    const id = req.params.id;
+    console.log(id);
+    const filter = {_id: new ObjectId(id)}
+    const result = await addCollection.findOne(filter);
+    res.send(result)
+  }) 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
